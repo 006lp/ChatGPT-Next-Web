7d7819dc9487f662a9cd613382221fcb8cbfbbfa
006lp <144674902+006lp@users.noreply.github.com>
2024-11-01 23:30:20 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -9,12 +9,12 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? `😆 对话遇到了一些问题，不用慌:
-       \\ 1️⃣ 想要零配置开箱即用，[点击这里立刻开启对话 🚀](${SAAS_CHAT_UTM_URL})
-       \\ 2️⃣ 如果你想消耗自己的 OpenAI 资源，点击[这里](/#/settings)修改设置 ⚙️`
+       \\ 请联系网站管理员@006lp寻求帮助
+       \\ 联系方式：[Telegram](https://t.me/lpsuki)、[X(原Twitter)](https://x.com/006lpsuki)或[Email](mailto:kirakirai8023@gmail.com)`
       : `😆 对话遇到了一些问题，不用慌:
-       \ 1️⃣ 想要零配置开箱即用，[点击这里立刻开启对话 🚀](${SAAS_CHAT_UTM_URL})
-       \ 2️⃣ 如果你正在使用私有部署版本，点击[这里](/#/auth)输入访问秘钥 🔑
-       \ 3️⃣ 如果你想消耗自己的 OpenAI 资源，点击[这里](/#/settings)修改设置 ⚙️
+       \ 看起来你并没有登录呢，点击[这里](/#/auth)输入访问秘钥 🔑
+       \ 没有秘钥？请联系网站管理员@006lp寻求帮助
+       \ 联系方式：[Telegram](https://t.me/lpsuki)、[X(原Twitter)](https://x.com/006lpsuki)或[Email](mailto:kirakirai8023@gmail.com)
        `,
   },
   Auth: {
